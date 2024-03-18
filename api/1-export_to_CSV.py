4e352c895efe0d6290fa1c4f3e9850abdb15000f
casey paul <casey.paul@atlasschool.com>
2024-03-18 20:07:56 +0000

    redo task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,27 +1,73 @@
 #!/usr/bin/python3
 """
-place holder
+Retrieves employee data from a REST API
+in CSV format displays info.
 """
+import csv
+import requests
+import sys
+
+
+def export_csv_data(tasks, user_id, employee_name):
+    filename = f"{user_id}.csv"
+
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([
+            "USER_ID",
+            "USERNAME",
+            "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"
+            ])
+
+        for task in tasks:
+            task_id = task['id']
+            task_title = task['title']
+            task_completed = task['completed']
+            task_status = "COMPLETED" if task_completed else "NOT COMPLETED"
+
+            writer.writerow([user_id, employee_name, task_status, task_title])
+
+        print(f"Data has been written to USER_ID.csv")
+
+
+def get_employee_list(id):
+    name_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+
+    response_name = requests.get(name_url)
+    response_todo = requests.get(todo_url)
+
+    if response_name.status_code == 200 and response_todo.status_code == 200:
+        employee_data = response_name.json()
+        todos = response_todo.json()
+
+        employee_name = employee_data['name']
+        total_tasks = len(todos)
+        completed_tasks = [
+            todo['title']
+            for todo in todos
+            if todo['completed']
+        ]
+
+        print(f"Employee {employee_name} is done with tasks"
+              f"({len(completed_tasks)}/{total_tasks}):")
+        print(f"EMPLOYEE_NAME:{employee_name}")
+        print(f"NUMBER_OF_DONE_TASKS:{len(completed_tasks)}")
+        print(f"TOTAL_NUMBER_OF_TASKS:{total_tasks}")
+        print("Completed Task Titles:")
+        for title in completed_tasks:
+            print(f"\t{title}")
+    else:
+        print("Failed to retrieve data.")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: 1-export_to_CSV.py <tasks> <user_id> <employee_name>")
+        sys.exit(1)
 
-    import csv
-    import requests
-    from sys import argv
-
-    if len(argv) < 2:
-        exit()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(argv[1]))
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}".format(argv[1]))
-    name = name.json()
-    name = name[0]["username"]
-    todos = todos.json()
-    file_name = "{}.csv".format(argv[1])
-    with open(file_name, 'w') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow([argv[1], name, todo['completed'], todo['title']])
+    tasks = sys.argv[1]
+    user_id = sys.argv[2]
+    employee_name = sys.argv[3]
+    export_csv_data(tasks, user_id, employee_name)
